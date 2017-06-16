105ef8c29db6bea1ba826977ee63ad168accb49b
Johnny John Johnnson <yoann.audon@gmail.com>
2017-06-16 18:35:12 +0200

    Mise à jour et ajout de nikto

--- disc-infra.py
+++ disc-infra.py
@@ -7,35 +7,46 @@ print("* Nmap")
 print("* Dnsmap")
 print("* SslScan")
 print("* Dirb")
+print("* Nikto")
+
 
 #Domain ?
 host = raw_input("Domain ? ")
+output_dir = raw_input("Output Directory ? ")
 
 # NMAP
 def nmap():
-	nmap = "nmap -sC -sS -sV -p 1-65535 -O -v --max-scan-delay 25 "
+	nmap = "nmap -sC -sS -sV -p 1-65535 -O -v --max-scan-delay 25 -oA nmap "
 	print("Command : ") + nmap, host
-	os.system("nmap"" " + str(host))
+	os.system(nmap + ' ' + host)
 nmap()
 
 # DNSMAP
 def dnsmap():
-	dnsmap = "dnsmap"
-	print("Command : ") + dnsmap, host
-	os.system("dnsmap"" " + str(host))
+        dnsmap = "dnsmap"
+        prefix = "-c dnsmap.csv"
+        print("Command : ") + dnsmap, host, prefix
+        os.system(dnsmap+' '+host+' '+prefix)
 dnsmap()
 
-#sslscan
+#SSLSCAN
 def ssl():
-	ssl = "sslscan"
+	ssl = "sslscan --xml=sslscan.xml"
 	print("Command : ") + ssl, host
-	os.system("sslscan"" " + str(host))
+	os.system(ssl+' '+host)
 ssl()
 
-#dirb
+#DIRB
 def dirb():
 	dirb = "dirb"
 	host2='https://'+host+' /usr/share/wordlists/dirb/common.txt'
 	print("Command : ") + dirb, host2
-	os.system("dirb"" " + str(host2))
+	os.system(dirb+' '+host2+' '">dirb.txt")
 dirb()
+
+#NIKTO
+def nikto():
+        nikto = "nikto -output nikto.html -h "
+        print("Command : ") + nikto, host
+        os.system(nikto+' '+host)
+nikto()
